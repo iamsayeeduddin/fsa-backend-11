830527fdc737e92697b6db72cba6648b44d1b74b
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-09-17 19:27:15 +0530

    DB Crud and Pagination

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,9 +1,29 @@
 import { ProductModel } from "../models/productModel.js";
 
+// PAGINATION
+// PAGE, PAGE SIZE (LIMIT), TOTAL RECORDS
+// 100, 10, 10
+// 105, 10, 11
+// records/pageSize =
+// 100, 10, 11 -> 3
+// (page - 1) * pageSize = SKIP VALUE
+
 const getAllProducts = async (req, res) => {
   try {
     /// SELECT * FROM "PRODUCTS";
     const products = await ProductModel.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
+
+const getPaginatedProducts = async (req, res) => {
+  try {
+    const page = +req.params.page;
+    const pageSize = +req.params.pageSize;
+    const skip = (page - 1) * pageSize;
+    const products = await ProductModel.find().skip(skip).limit(pageSize);
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error!" });
@@ -23,4 +43,27 @@ const addProduct = async (req, res) => {
   }
 };
 
-export { getAllProducts, addProduct };
+const updateProduct = async (req, res) => {
+  try {
+    const data = req.body;
+    const productId = req.params.id;
+    const product = await ProductModel.findByIdAndUpdate({ _id: productId }, data, { new: true });
+    let resp = { success: true, message: "Prduct updated Successfully!", data: product };
+    res.status(200).json(resp);
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error!" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    await ProductModel.findByIdAndDelete({ _id: productId });
+    let resp = { success: true, message: "Prduct deleted Successfully!" };
+    res.status(200).json(resp);
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error!" });
+  }
+};
+
+export { getAllProducts, addProduct, updateProduct, deleteProduct, getPaginatedProducts };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAllProducts, addProduct } from "../controllers/productCtrl.js";
+import { getAllProducts, addProduct, updateProduct, deleteProduct, getPaginatedProducts } from "../controllers/productCtrl.js";
 
 const router = express.Router();
 
 router.get("/allProducts", getAllProducts);
+router.get("/allProducts/:page/:pageSize", getPaginatedProducts);
 router.post("/addProduct", addProduct);
+router.patch("/updateProduct/:id", updateProduct);
+router.delete("/deleteProduct/:id", deleteProduct);
 
 export default router;
